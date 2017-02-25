b497a3261d7fb4591c8f7465650a12ac3a438d02
MicrocontrollersAndMore <cdahms123@gmail.com>
2017-02-24 22:40:42 -0500

    initial commit to add Visual Studio project

---
+++ frmMain.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ImageBox1 = New Emgu.CV.UI.ImageBox()
+        CType(Me.ImageBox1,System.ComponentModel.ISupportInitialize).BeginInit
+        Me.SuspendLayout
+        '
+        'ImageBox1
+        '
+        Me.ImageBox1.Location = New System.Drawing.Point(108, 104)
+        Me.ImageBox1.Name = "ImageBox1"
+        Me.ImageBox1.Size = New System.Drawing.Size(492, 388)
+        Me.ImageBox1.TabIndex = 2
+        Me.ImageBox1.TabStop = false
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8!, 16!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(729, 543)
+        Me.Controls.Add(Me.ImageBox1)
+        Me.Name = "frmMain"
+        Me.Text = "Form1"
+        CType(Me.ImageBox1,System.ComponentModel.ISupportInitialize).EndInit
+        Me.ResumeLayout(false)
+
+End Sub
+
+    Friend WithEvents ImageBox1 As Emgu.CV.UI.ImageBox
+End Class
